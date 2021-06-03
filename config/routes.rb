9d6c28f2c6ac3767130077aece6d78d629f86c94
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-03 00:52:22 +0000

    [終了]Bookers2にメール送信機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   resources :users,only: [:show,:index,:edit,:update] do
     member do
